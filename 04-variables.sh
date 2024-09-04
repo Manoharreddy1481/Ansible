9f0632bba6a8bcb9ad192b7981a9a7b8d1fb5ef3
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-04 23:34:13 +0530

    Ansible

---
+++ 04-variables.sh
@@ -0,0 +1,14 @@
+- name: Play-01
+  hosts: web #which hosts your ansible wants to connect
+  vars:
+    NAME: Manohar
+  tasks: #list of tasks/modules/collections
+  - name: ping the server
+    ansible.builtin.debug:
+      msg: "Name is {{NAME}}"
+- name: Play-02
+  hosts: web #which hosts your ansible wants to connect
+  tasks: #list of tasks/modules/collections
+  - name: ping the server
+    ansible.builtin.debug:
+      msg: "Name is {{NAME}}"
